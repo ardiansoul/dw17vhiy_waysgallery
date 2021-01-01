932f319595552757a7b9582451f8bebe362b37c7
ardiansoul <yanaardi523@gmail.com>
2021-01-01 15:16:12 +0700

    fixing auto login

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -10,6 +10,13 @@ const initialState = {
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
+    case "USER_LOADED":
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        isLogin: true,
+        user: action.payload,
+      };
     case "LOGIN":
       localStorage.setItem("token", action.payload.token);
       return {
@@ -17,6 +24,7 @@ const reducer = (state, action) => {
         user: action.payload,
         isLogin: true,
       };
+    case "AUTH_ERROR":
     case "LOGOUT":
       localStorage.removeItem("token");
       return {

a111a50c12f1ab5e010945626f5643a743b62b07
ardiansoul <yanaardi523@gmail.com>
2021-01-01 16:46:30 +0700

    fixing token options

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,16 +1,11 @@
 export const baseUrl = "https://dw17vhiy-waysgallery-api.herokuapp.com/";
 export const options = {
   headers: `${
-    localStorage.token
-      ? {
-          Authorization: localStorage.token,
+    localStorage.token && {
+      Authorization: localStorage.token,
 
-          withCredentials: true,
-          "Access-Control-Allow-Origin": "*",
-        }
-      : {
-          withCredentials: true,
-          "Access-Control-Allow-Origin": "*",
-        }
+      withCredentials: true,
+      "Access-Control-Allow-Origin": "*",
+    }
   }`,
 };
